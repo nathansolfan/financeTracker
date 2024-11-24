63ea071926eef9775eaf8e44af59b14717d5b7ea
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-24 23:28:44 +0000

    updates

--- database/migrations/2024_11_24_231212_create_expenses_table.php
+++ database/migrations/2024_11_24_231212_create_expenses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('casdade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->decimal('amount', 10, 2);
             $table->string('category');
             $table->string('description');
