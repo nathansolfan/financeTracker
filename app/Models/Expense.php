745fb48d141796526d267d43c40bbff69aa43a30
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-25 12:04:46 +0000

    update models

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    // expense belongs to a specific USER
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Income.php
+++ app/Models/Income.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Income extends Model
 {
-    //
+    // INCOME belongsTo the MODEL USER::
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
