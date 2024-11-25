d074eaa1a8620ec3407b1753095401c2ed5f9889
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-25 15:38:59 +0000

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BudgetController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\GroceryListController;
+use App\Http\Controllers\IncomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +19,30 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth', function () {
+
+    // Expense Routes
+    Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+    Route::get('/expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+
+    // Income Route
+    Route::get('/incomes', [IncomeController::class, 'index'])->name('incomes.index');
+    Route::get('/incomes/create', [IncomeController::class, 'create'])->name('incomes.create');
+    Route::post('/incomes', [IncomeController::class, 'store'])->name('incomes.store');
+
+    // Budget Route
+    Route::get('/budget', [BudgetController::class, 'index'])->name('budget.index');
+    Route::get('/budget/create', [BudgetController::class, 'create'])->name('budget.create');
+    Route::post('/budget', [BudgetController::class, 'store'])->name('budget.store');
+
+    // Grocery Route
+    Route::get('/grocery', [GroceryListController::class, 'index'])->name('grocery.index');
+    Route::get('/grocery/create', [GroceryListController::class, 'create'])->name('grocery.create');
+    Route::post('/grocery', [GroceryListController::class, 'store'])->name('grocery.store');
 });
 
 require __DIR__.'/auth.php';
