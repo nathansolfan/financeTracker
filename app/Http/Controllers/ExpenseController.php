9eb1fe3a93c237492359ed1b77d090c5146d97c2
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-27 22:47:59 +0000

    updates

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\ChartService;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -42,7 +42,6 @@ class ExpenseController extends Controller
 
     public function store(Request $request)
     {
-
         $validated = $request->validate([
             'amount' => 'required|numeric|min:0.01',
             'category' => 'required|string|max:255',
@@ -53,5 +52,38 @@ class ExpenseController extends Controller
         $user->expenses()->create($validated);
 
         return redirect()->route('expenses.index')->with('success', 'Expensed added!');
+    }
+
+    public function edit($id)
+    {
+        $user = Auth::user();
+        $expense = $user->expenses()->findOrFail($id);
+
+        return view('expenses.edit', compact('expense'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'amount' => 'required|numeric|min:0.01',
+            'category' => 'required|string|max:255',
+            'description' => 'nullable|string|max:255',
+            'date' => 'required|date',
+        ]);
+
+        $user = Auth::user();
+        $expense = $user->expenses()->findOrFail($id);
+        $expense->update($validated);
+
+        return redirect()->route('expenses.index')->with('success', 'Expense has been updated');
+    }
+
+    public function destroy($id)
+    {
+        $user = Auth::user();
+        $expense = $user->expenses()->findOrFail($id);
+        $expense->delete();
+
+        return redirect()->route('expenses.index')->with('success', 'Expense hasbeen deleted');
     }
 }
